7c2e21c5f77865f4a401636c0dd81afcf2b5c13a
Medet Ahmetson <admin@blocklords.io>
2023-08-01 17:46:05 +0300

    destination is running

--- destination.go
+++ destination.go
@@ -1,5 +1,10 @@
 package main
 
+//
+// A destination should be another service.
+// Here, we define it for example only. So we can run this proxy without relying on another service.
+//
+
 import (
 	"github.com/ahmetson/service-lib/configuration"
 	"github.com/ahmetson/service-lib/controller"
@@ -7,9 +12,7 @@ import (
 )
 
 // creates a sample destination controller
-func newDestination(destinationConfig configuration.Controller, serviceUrl string) *controller.Controller {
-
-	logger, _ := log.New("destination", false)
+func newDestination(destinationConfig configuration.Controller, serviceUrl string, logger *log.Logger) *controller.Controller {
 	destination, _ := controller.SyncReplier(logger)
 	destination.AddConfig(&destinationConfig, serviceUrl)
 
